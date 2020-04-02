72595d5cdbf8ac7888b1ef5f0ffbbaf209f5da9e
KrastevIT <barcodebit000@gmai.com>
2020-04-02 11:55:20 +0300

    Add User

---
+++ KTaseva/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KTaseva.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
