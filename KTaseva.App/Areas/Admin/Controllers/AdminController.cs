6dbfac6ff4588eb475e7a4b255fad8ed759bbc5b
KrastevIT <barcodebit000@gmai.com>
2020-04-07 17:51:10 +0300

    Add Area Admin

---
+++ KTaseva.App/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KTaseva.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public abstract class AdminController : Controller
+    {
+    }
+}
--- KTaseva.App/Startup.cs
+++ KTaseva.App/Startup.cs
@@ -86,6 +86,10 @@ namespace KTaseva.App
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                   name: "areas",
+                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
