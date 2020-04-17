6c172b70e8f947b588550d88b2c7cb442f0c07c2
KrastevIT <barcodebit000@gmai.com>
2020-04-17 17:58:32 +0300

    Add Tests GetDisabledDatesReturnInvalid

--- KTaseva.Tests/Services/Appointments/GetDisabledDatesTests.cs
+++ KTaseva.Tests/Services/Appointments/GetDisabledDatesTests.cs
@@ -40,6 +40,29 @@ namespace KTaseva.Tests.Services.Appointments
 
             Assert.Contains(expected, dates);
         }
+
+        [Theory]
+        [InlineData(-1, "dd.MM.yyyy")]
+        [InlineData(+2, "dd.MM.yyyy")]
+        [InlineData(-1, "yyyy.dd.MM")]
+        public void GetDisabledDatesReturnInvalid(int addDays, string dateFormat)
+        {
+            var disabledDate = new DisableDate
+            {
+                Id = 1,
+                DisabledDates = DateTime.Today.AddDays(+1)
+            };
+
+            this.db.DisableDates.Add(disabledDate);
+            this.db.SaveChanges();
+
+            var dates = this.appointmentService.GetDisabledDates();
+
+            var date = DateTime.Today.AddDays(addDays).ToString(dateFormat);
+            var expected = JsonConvert.SerializeObject(date);
+
+            Assert.DoesNotContain(expected, dates);
+        }
     }
 }
 
