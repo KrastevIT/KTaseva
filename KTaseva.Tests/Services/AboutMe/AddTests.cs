e4f48b320a67386fdfa320ae088badc92eb585e9
KrastevIT <barcodebit000@gmai.com>
2020-04-21 18:23:32 +0300

    Add Test AboutMeService AddReturnCorrectly

---
+++ KTaseva.Tests/Services/AboutMe/AddTests.cs
@@ -0,0 +1,34 @@
+﻿using KTaseva.Data;
+using KTaseva.Services.AboutMe;
+using KTaseva.Tests.Configurations;
+using KTaseva.ViewModels.AboutMe;
+using System.Linq;
+using Xunit;
+
+namespace KTaseva.Tests.Services.AboutMe
+{
+    public class AddTests
+    {
+        private readonly KTasevaDbContext db;
+        private readonly AboutMeService aboutMeService;
+
+        public AddTests()
+        {
+            this.db = new KTasevaDbContext(MemoryDatabase.OptionBuilder());
+            this.aboutMeService = new AboutMeService(db);
+        }
+
+        [Fact]
+        public void AddReturnCorrectly()
+        {
+            var model = new AboutMeInputModel
+            {
+                Id = 1
+            };
+
+            this.aboutMeService.Add(model);
+
+            Assert.Equal(1, this.db.AboutMe.Count());
+        }
+    }
+}
